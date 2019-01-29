68281d4cf027e2215536e1b7c0219e041618362e
Ivan Liang <ivanwenliang@gmail.com>
2019-01-28 22:34:26 -0800

    Added Heroku postgresql

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-clean-30320',
         user: 'IvanLiang',
         password: '',
         database: 'smart-brain'
